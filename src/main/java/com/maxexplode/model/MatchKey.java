53ddb822aeeaac6c93e91c72b3f3aa76926af027
maxexplode <56011332+maxexplode@users.noreply.github.com>
2025-05-04 13:04:17 +0800

    Avoid unintended changes for MatchKey

--- src/main/java/com/maxexplode/model/MatchKey.java
+++ src/main/java/com/maxexplode/model/MatchKey.java
@@ -1,4 +1,26 @@
 package com.maxexplode.model;
 
+import java.util.Objects;
+
 public record MatchKey(String homeTeam, String awayTeam) {
+
+
+    //Location
+    //Team Category
+
+    //Addition or attrs should not change the behavior of hash and equals unintentionally
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof MatchKey other)) return false;
+        return Objects.equals(homeTeam, other.homeTeam) &&
+                Objects.equals(awayTeam, other.awayTeam);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(homeTeam, awayTeam);
+    }
+
 }
